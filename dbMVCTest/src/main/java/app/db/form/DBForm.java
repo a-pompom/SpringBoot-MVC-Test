d0c7586079de54adb5c530b2529284e490c21966
a-pompom <aoi.matsuda.3x@gmail.com>
2019-08-12 14:53:10 +0900

    テストのための土台作成 #1

---
+++ dbMVCTest/src/main/java/app/db/form/DBForm.java
@@ -0,0 +1,26 @@
+package app.db.form;
+
+import java.util.List;
+
+import app.db.entity.User;
+
+/**
+ * ユーザエンティティのリストを格納するためのフォーム
+ * @author aoi
+ *
+ */
+public class DBForm {
+	
+	private List<User> userList;
+
+	public List<User> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+	
+	
+
+}
