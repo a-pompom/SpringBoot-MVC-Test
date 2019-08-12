a72f1fdf095d40454f178d5549343b4901532703
a-pompom <aoi.matsuda.3x@gmail.com>
2019-08-12 17:21:51 +0900

    helloコントローラコメント整理

--- dbMVCTest/src/test/java/app/db/test/HelloControllerTest.java
+++ dbMVCTest/src/test/java/app/db/test/HelloControllerTest.java
@@ -18,28 +18,46 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import app.db.controller.HelloController;
 
+/**
+ * @ExtendWith...JUnit5でテストクラスを実行させるために必要なアノテーション
+ * @AutoConfigureMvc...テストクラスでMockMvcを利用するために必要なアノテーション
+ * @SpringBootTest...application.propertiesなどのSpringBootアプリ用の設定、
+ * 	    classesプロパティに設定したクラスをDIコンテナから取得し、利用するためのアノテーション
+ * @author aoi
+ *
+ */
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest(classes = HelloController.class)
 public class HelloControllerTest {
 	
+	//mockMvc TomcatサーバへデプロイすることなくHttpリクエスト・レスポンスを扱うためのMockオブジェクト
 	@Autowired
 	private MockMvc mockMvc;
 	
-	
+	// getリクエストでviewを指定し、httpステータスでリクエストの成否を判定
 	@Test
-	void init処理が走って2000が返る() throws Exception {
-		
+	void init処理が走って200が返る() throws Exception {
+		// andDo(print())でリクエスト・レスポンスを表示
+		// ビューのレンダリング結果までは得られないので、画面上の確認はここではできない
 		this.mockMvc.perform(get("/hello/init")).andDo(print())
 			.andExpect(status().isOk());
 	}
 	
+	// リクエストスコープに渡されるモデルを検証する
+	// 簡単なアプリを作っていて起こるバグの大半は、モデルへ適切な値が渡されなかったことによるものなので、
+	// モデルの検証は重要になると思われる
 	@Test
 	void init処理でモデルのメッセージにhelloが渡される() throws Exception {
 		this.mockMvc.perform(get("/hello/init"))
 			.andExpect(model().attribute("message", "hello!"));
 	}
 	
+	// リクエストスコープへ渡されたモデルの中で、入れ子となっている、かつリスト構造を持つものについて
+	// 検証を行う。 ネストされたものについては、「hasProperty」でプロパティの存在検証を行い、
+	// 最下層まで到達することで値の検証が可能となる。
+	// リスト要素については、hasItemで順番を問わずリストへアクセスし、指定されたプロパティが指定の値となる要素が存在するかを検証。
+	// 存在する場合のみテストをグリーンとする
 	@Test
 	void init処理でモデルのフォームへユーザリストが格納されるか() throws Exception {
 		
