3bbc156d3cdc5e3e5de1f0a03c4e1e23877880e2
a-pompom <aoi.matsuda.3x@gmail.com>
2019-08-13 16:38:38 +0900

    POSTリクエスト後のDBの状態を検証可能とした #1

--- dbMVCTest/src/test/java/app/db/test/DBCrudTest.java
+++ dbMVCTest/src/test/java/app/db/test/DBCrudTest.java
@@ -4,15 +4,23 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
@@ -25,8 +33,10 @@ import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
 import app.db.controller.DBCrudController;
+import app.db.entity.User;
 import app.db.main.DbMvcTestApplication;
 
 @ExtendWith(SpringExtension.class)
@@ -46,13 +56,11 @@ public class DBCrudTest {
 	private MockMvc mockMvc;
 	
 	
-	
-	@Test
 	@DatabaseSetup(value = "/testData/")
 	@Transactional
 	void ステータス200が返る() throws Exception {
 		
-		this.mockMvc.perform(get("/crud/init")).andDo(print())
+		this.mockMvc.perform(get("/crud/init"))
 			.andExpect(status().isOk())
 			.andExpect(model().attribute("dbForm", hasProperty(
 					"userList", hasItem(
@@ -65,10 +73,12 @@ public class DBCrudTest {
 	
 	@Test
 	@DatabaseSetup(value = "/testData/")
-	@ExpectedDatabase(value = "/expectedData/")
-	@Transactional
+	@ExpectedDatabase(value = "/expectedData/", assertionMode=DatabaseAssertionMode.NON_STRICT)
 	void フォームの入力値でDBが更新される() throws Exception {
 		
+		this.mockMvc.perform(post("/crud/save/0/1")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("userList[0].userName", "test1mod")).andDo(print());
 	}
 	
 	
